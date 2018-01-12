b519f8d62db998d8492fa5ceba4b38194151c610
Moacir Juster <moacirjunior.eletro@gmail.com>
2018-01-12 09:19:51 -0400

    formatado arquivo mast-c1.php

--- mast-c1.php
+++ mast-c1.php
@@ -1,15 +1,9 @@
 <?php
 
-<<<<<<< HEAD
 echo "<a href='https://www.facebook.com/moacirjun'>Moacir</a>";
 
->
+?>
 
 <a href="https://:qgoogle.com.br"></a>
-=======
-echo "<a href='https://www.facebook.com/moacirjun'>Moacir</a>"
-
->
-
 <a href="https://facebook.com">Facebook</a>
->>>>>>> add link facebook
+
