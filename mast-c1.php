26d260412da439e262f15c286b8bfb140d39c6ca
Moacir Juster <moacirjunior.eletro@gmail.com>
2018-01-11 23:34:12 -0400

    add link facebook

--- mast-c1.php
+++ mast-c1.php
@@ -1,7 +1,15 @@
 <?php
 
+<<<<<<< HEAD
 echo "<a href='https://www.facebook.com/moacirjun'>Moacir</a>";
 
 >
 
 <a href="https://:qgoogle.com.br"></a>
+=======
+echo "<a href='https://www.facebook.com/moacirjun'>Moacir</a>"
+
+>
+
+<a href="https://facebook.com">Facebook</a>
+>>>>>>> add link facebook
